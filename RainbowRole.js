d45b0d002e253dedd15f63a99b7571f7fcc0df19
contatoivituucom <39351177+contatoivituucom@users.noreply.github.com>
2018-06-10 19:09:39 -0300

    Update RainbowRole.js

--- RainbowRole.js
+++ RainbowRole.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const prefix = 'adv:';
+const prefix = '!!';
 
 client.on("message", msg => {
 if (msg.content.startsWith(prefix + "anunciar")) {
